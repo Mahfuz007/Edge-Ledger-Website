a6c0ec4995282b4ba9b0b8f7803691b80a5902e6
Mahfuz <aamahfuz2@gmail.com>
2020-05-15 13:29:00 +0600

    initial commit

---
+++ js/main.js
@@ -0,0 +1,34 @@
+var map;
+function initMap() {
+    const loc={lat: 23.8907, lng: 89.1099};
+    map = new google.maps.Map(document.querySelector('.contact-map'), {
+    center: loc,
+    zoom: 8
+    });
+
+    const marker = new google.maps.Marker({position: loc,map:map})
+}
+
+// Sticky menu Background
+window.addEventListener('scroll',function(){
+    if(window.scrollY>150){
+        document.querySelector('#navbar').style.opacity = 0.9;
+    }else{
+        document.querySelector('#navbar').style.opacity = 1;
+    }
+});
+
+// Smooth Scrolling
+
+$('#navbar a, .btn').on('click',function(event){
+    if(this.hash!==''){
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top-100
+        },
+        800
+        );
+    }
+});
